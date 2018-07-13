0340d04432e0b411d3ba72f272096b58afa746d4
Wesley Monaro <wesleymonaro@hotmail.com>
2018-07-12 21:42:55 -0300

    Trabalhando com Subjects

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
+// var subject = new Rx.Subject();
+
+// var subscription = subject.subscribe(
+//     value => console.log(`My value is ${value}`),
+//     err => console.log(`error ${err}`),
+//     () => console.log(`Completed`),
+// )
+
+// subject.next(55);
+// subject.next(34);
+// subject.complete();
+
 var observable = Rx.Observable.create(observer => {
-    var i = 0;
-    setInterval(() => {
-        observer.next(i++)
-        if (i > 5) observer.complete()
-    }, 1000)
+    var i = 0,
+        interval = setInterval(() => {
+            i++;
+            observer.next(parseInt(Math.random() * 10));
+            if (i > 5) {
+                clearInterval(interval);
+                observer.complete();
+            }
+        }, 1000)
 })
 
-observable.subscribe(
-    value => console.log(`My Value is ${value}`),
+var subject = new Rx.Subject();
+
+var subSource = observable.subscribe(subject)
+
+var subsription = subject.subscribe(
+    value => console.log(`My value is ${value}`),
     err => console.log(`error ${err}`),
     () => console.log(`Completed`),
 )
